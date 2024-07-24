2638d36c014c6e95d43358856dcae4500bc499db
James Xia <113453761+jamsxia@users.noreply.github.com>
2024-07-24 18:42:41 -0400

    Add files via upload
    test decimal places

--- assignment3/hopfield.py
+++ assignment3/hopfield.py
@@ -9,16 +9,29 @@ class Hopfield():
         self.n=n
         self.T=np.zeros((n,n))
 
-    def learn(data):
-        for a in data:
-            
+    def learn(self, data):
 
-    def test():
-        pass
-def show_confustion(arr1, arr2):
+        # Accumulate weights over patterns
+        for a in data:
+
+                self.T += np.outer(2 * a - 1, 2 * a - 1)
+
+        # Zero-out the diagonal (no self connections!)
+        np.fill_diagonal(self.T, 0)
+
+
+    def test(self, pattern, niter=5):
+        for iter in range(niter):
+            #pattern+=np.outer(pattern, self.T)
+            for j in range(len(pattern)):
+                for i in range(j+1, len(pattern)):
+                    pattern[j]+=pattern[i]*self.T[i,j]
+        return np.round(pattern)
+
+def show_confusion(arr1, arr2):
     for i in range(len(arr1)):
         a=arr1[i]
-        for j in range(len(arr2)):
+        for j in range(i+1):
             b=arr2[j]
             cos=np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
             if(cos!=0):
@@ -30,12 +43,34 @@ def noisy_copy(arr, prob):
     outputArr=np.where(probArr<prob, 1-arr, arr)
     return outputArr
 
+def random_array(rows, cols):
+
+    return np.round(np.random.random((5, 30)))
+
+
 def main():
-    data = np.round(np.random.random((5, 30)))
-    #data = np.random.random((5, 30))
-    #print(np.linalg.norm(data[1]))
-    #show_confustion(data,data)
-    print(noisy_copy(data, 0))
+
+    print('Part 1: Generate some training data --------------------------------------------------')
+    data = random_array(5, 30)
+    print(data)
+
+    print('\nPart 2: Vector-cosine confusion matrix of an array with itself ---------------------')
+    show_confusion(data, data)
+
+    print('\nPart 3: Confusion matrix with 25% noise -----------------------------------------------')
+    show_confusion(data, noisy_copy(data, 0.25))
+
+    print('\nPart 4: Recovering small patterns with a Hopfield net -----------------------------------')
+    net = Hopfield(30)
+    net.learn(data)
+    testData=np.round(np.random.random(30))
+    print("Input:"+np.array_str(testData))
+    print("Output:"+np.array_str(net.test(testData)))
+    print('\n\nRecover pattern, no noise:')
+    noiseData=noisy_copy(testData,0.25)
+    print("Input:"+np.array_str(noiseData))
+    print("Output:"+np.array_str(net.test(noiseData)))
+    print("Original:"+np.array_str(testData))
 
 if __name__ =="__main__":
     main()
