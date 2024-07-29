dcc70a380ebbe3b7c8f7aa8020751fbd91ab543f
James Xia <113453761+jamsxia@users.noreply.github.com>
2024-07-29 14:16:27 -0400

    Add files via upload
    finish hopfield neuromorphic

--- assignment3/hopfield.py
+++ assignment3/hopfield.py
@@ -20,16 +20,15 @@ class Hopfield():
         np.fill_diagonal(self.T, 0)
 
 
-    def test(self, pattern, niter=5):
-        copy_data=np.copy(pattern)
-        for iter in range(niter):
-            #pattern+=np.outer(pattern, self.T)
-            for i in range(len(pattern)):
-                for j in range(i+1, len(pattern)):
-                    copy_data[j]+=copy_data[i]*self.T[i,j]
-        #return (np.rint(copy_data))
-        #return np.round(copy_data,1)
-        return np.where(copy_data>=0, 1, 0)
+    def test(self, u, niter=5):
+
+        for _ in range(niter):
+
+                u = (np.dot(u, self.T)>=0).astype('int')
+
+        return u
+
+     
 def cosine(a,b):
     return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
 
@@ -54,11 +53,12 @@ def random_array(rows, cols):
     return np.round(np.random.random((rows, cols)))
 
 
+
 def main():
 
     print('Part 1: Generate some training data --------------------------------------------------')
     data = random_array(5, 30)
-    print(data)
+    print(data.astype('int'))
 
     print('\nPart 2: Vector-cosine confusion matrix of an array with itself ---------------------')
     show_confusion(data, data)
@@ -70,17 +70,17 @@ def main():
     net = Hopfield(30)
     net.learn(data)
     print('\n\nRecover pattern, no noise:') 
-    testData=np.round(np.random.randint(2,size=30))
-    print("Input: "+np.array_str(testData))
+    testData=data[0]
+    print("Input: "+np.array_str(testData.astype('int')))
     print("Output:"+np.array_str(net.test(testData)))
     print("Vector cosine=",'%3.2f '%cosine(testData,net.test(testData)))
     print('\n\nRecover pattern, 25% noise:')
     noiseData=noisy_copy(testData,0.25)
-    print("Input:   "+np.array_str(noiseData))
-    print("Output:  "+np.array_str(net.test(noiseData)))
-    print("Original:"+np.array_str(testData))
-    print("Vector cosine=",'%3.2f '%cosine(testData,net.test(noiseData)))
-'''
+    print("Input:   " + str(noiseData.astype('int')))
+    print("Output:  " + str(net.test(noiseData)))
+    print("Original:" + str(testData.astype('int')))
+    print("Vector cosine=",'%3.2f '% cosine(testData,net.test(noiseData)))
+
     print("Part 5: Recovering big patterns ----------------------------------------------------")
     newData = random_array(10, 1000)
     print("Confusion matrix for 1000-element vectors with 25% noise")
@@ -88,7 +88,6 @@ def main():
     print('\n\nRecover pattern, no noise:')
     for i in range(len(newData)):
         print("Vector cosine on pattern %s = %s"%(str(i), str(cosine(testData,net.test(testData)))))
-'''
 
 
 
